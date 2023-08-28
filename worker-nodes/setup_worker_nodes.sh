6d52d9d430d7bd7bbbe74650721ba4eca471bdb3
Cu112145 <Cu112145@gmail.com>
2023-08-28 03:25:17 -0600

    deploy kube dashboard

--- worker-nodes/setup_worker_nodes.sh
+++ worker-nodes/setup_worker_nodes.sh
@@ -10,6 +10,6 @@ worker_names=(
 )
 
 for name in "${worker_names[@]}"; do
-    start_command="sudo kubeadm join 135.181.246.248:6443 --token cabdka.skujkk3oz4d6jffj --discovery-token-ca-cert-hash sha256:ae2adf096ca5882939922bcc4aa998cc03a2d08abf913a6a501338d187f75874 --node-name=$name"
+    start_command="kubeadm join 135.181.246.248:6443 --token cabdka.skujkk3oz4d6jffj --discovery-token-ca-cert-hash sha256:ae2adf096ca5882939922bcc4aa998cc03a2d08abf913a6a501338d187f75874 --node-name=$name"
     docker run -it --privileged --network host kubelet-container /bin/bash -c "$start_command"
 done
