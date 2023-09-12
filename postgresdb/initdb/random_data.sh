4e4fec8bcafea4367f3e23c7658a135fb520c80b
Cu112145 <Cu112145@gmail.com>
2023-09-12 01:16:05 -0600

    updated postgres build

--- postgresdb/initdb/random_data.sh
+++ postgresdb/initdb/random_data.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
-PGPASSWORD=$POSTGRES_PASSWORD psql -U $POSTGRES_USER -d $POSTGRES_DB -c "
-DO $$ 
-BEGIN
-    FOR i IN 1..1000 LOOP
-        INSERT INTO employees (name, age) VALUES (md5(random()::text), floor(random() * (70-18+1) + 18)::integer);
-    END LOOP;
-END $$;
 
+SQL_COMMAND=$(cat <<- EOM
+    DO $$ 
+    BEGIN
+        FOR i IN 1..1000 LOOP
+            INSERT INTO employees (name, age)
+            VALUES (md5(random()::text), floor(random() * (70-18+1) + 18)::integer);
+        END LOOP;
+    END $$;
+EOM
+)
+
+PGPASSWORD="$POSTGRES_PASSWORD" psql -U "$POSTGRES_USER" -d "$POSTGRES_DB" -c "$SQL_COMMAND"
